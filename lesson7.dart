ca2633e8eb10d26477f3e8a09e7fe42fa26eff66
isafiulin <isafiulin@neotelecom.kg>
2022-11-29 19:51:59 +0600

    first commit

---
+++ lesson7.dart
@@ -0,0 +1,30 @@
+void main() {
+  // for (int i = 0; i < 6; i++) {
+  //   print(i);
+  // }
+
+  // int a = 10;
+  // print(a++); // постфиксный инкремент (постинкремент)
+
+  // int b = 10;
+  // print(++b); // префиксный инкремент (преинкремент)
+
+    int j = 10;
+
+  while (j <= 20) {
+    print(j);
+
+    j++;
+  }
+
+  int i = 10;
+  print("Dart do-while loop example");
+
+  do {
+    print(i);
+
+    i++;
+  } while (i <= 20);
+
+  print("The loop is terminated");
+}
